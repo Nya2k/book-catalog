715aa72709a48b4ec9f14a0af17f0b111d0655f9
Nya2k <eudoravanya@gmail.com>
2023-09-11 00:24:23 +0700

    changed view and edit template

--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Hujan',
-        'amount': '10',
-        'description': 'Novel Hujan merupakan novel yang mengisahkan kisah cinta serta perjuangan hidup Lail.'
+        'application': 'Book Catalog',
+        'name': 'Eudora Vanya Lindsay',
+        'class': 'PBP-D'
     }
 
     return render(request, "main.html", context)
