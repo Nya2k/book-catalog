72ad95a030048901c2e04bb9c5494a3eb650a79a
Nya2k <eudoravanya@gmail.com>
2023-09-11 23:20:18 +0700

    changed views and css to tailwind

--- main/views.py
+++ main/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'application': 'Book Catalog',
-        'name': 'Eudora Vanya Lindsay',
-        'class': 'PBP-D'
+        'application': 'BOOK CATALOG',
+        'name': 'Hujan',
+        'amount': '7',
+        'description': 'Novel Hujan merupakan novel yang mengisahkan kisah cinta serta perjuangan hidup Lail.'
     }
 
     return render(request, "main.html", context)
